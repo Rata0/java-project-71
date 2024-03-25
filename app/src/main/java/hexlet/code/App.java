b88bebad3458a0538c9d206c71830676d661d5c5
Alexander <nekon689@gmail.com>
2024-03-25 12:41:26 +0300

    Step 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,24 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "gendiff", version = "1.0",description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+    @Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
+    boolean helpRequired;
+
+    @Option(names = { "-V", "--version" }, versionHelp = true,description = "Print version information and exit.")
+    boolean versionRequested;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public void run() {
+       return;
     }
 }
