74c9947e5d217f51e4b631cb741bd9ade2b1df74
Alexander <nekon689@gmail.com>
2024-03-27 20:07:15 +0300

    add test json format

---
+++ app/src/test/java/DifferTest.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+import hexlet.code.Differ;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DifferTest {
+    @Test
+    public void testGenerateWithSameFiles() throws IOException {
+        String expectedDiff = "{\n"
+                + "  - follow: false\n"
+                + "    host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + timeout: 20\n"
+                + "  + verbose: true\n"
+                + "}";
+
+        String actualDiff = Differ.generate("src/test/resources/file1.json", "src/test/resources/file2.json");
+        assertEquals(expectedDiff, actualDiff);
+    }
+}
