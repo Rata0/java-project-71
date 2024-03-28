1719807fd252ec7faeffeed0a13b728c41d5c4a7
Alexander <nekon689@gmail.com>
2024-03-28 12:45:00 +0300

    add Test yaml

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
     @Test
-    public void testGenerateWithSameFiles() throws IOException {
+    public void testGenerateJSON() throws IOException {
         String expectedDiff = "{\n"
                 + "  - follow: false\n"
                 + "    host: hexlet.io\n"
@@ -17,6 +17,20 @@ public class DifferTest {
                 + "}";
 
         String actualDiff = Differ.generate("src/test/resources/file1.json", "src/test/resources/file2.json");
+        assertEquals(expectedDiff, actualDiff);
+    }
+
+    public void testGenerateYML() throws IOException {
+        String expectedDiff = "{\n"
+                + "  - follow: false\n"
+                + "    host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + timeout: 20\n"
+                + "  + verbose: true\n"
+                + "}";
+
+        String actualDiff = Differ.generate("src/test/resources/file1.yml", "src/test/resources/file2.yml");
         assertEquals(expectedDiff, actualDiff);
     }
 }
