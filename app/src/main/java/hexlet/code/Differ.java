c5962688b643956fa799ac11aa6d8324cad747c4
Alexander <nekon689@gmail.com>
2024-04-17 15:32:29 +0300

    fix code Differ and Parser

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,52 +2,21 @@ package hexlet.code;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class Differ {
     public static void main(String[] args) throws IOException {
-        String result = generate("src/test/resources/file1.yml", "src/test/resources/file2.yml");
-        System.out.println(result);
+        System.out.println(generate("src/test/resources/file1.json", "src/test/resources/file2.json", "stylish"));
+    }
+    public static String generate(String file1, String file2, String titleFormat) throws IOException {
+        Map<String, Object> dataFile1 = Parser.parse(file1);
+        Map<String, Object> dataFile2 = Parser.parse(file2);
+        ArrayList<HashMap<String, Object>> diffObj = BuildAST.generateDiffObj(dataFile1, dataFile2);
+        return Formatter.format(diffObj, titleFormat);
     }
 
     public static String generate(String file1, String file2) throws IOException {
-        Map<String, Object> dataFile1 = Parser.parser(file1);
-        Map<String, Object> dataFile2 = Parser.parser(file2);
-        List<String> differ = differenceMAP(dataFile1, dataFile2);
-        StringBuilder result = new StringBuilder("{").append("\n");
-        for (String line : differ) {
-            result.append(line).append("\n");
-        }
-        result.append("}");
-
-        return result.toString();
-    }
-
-    private static List<String> differenceMAP(Map<String, Object> map1, Map<String, Object> map2) {
-        List<String> result = new ArrayList<>();
-        TreeMap<String, Object> sortedMap1 = new TreeMap<>(map1);
-        TreeMap<String, Object> sortedMap2 = new TreeMap<>(map2);
-        for (Map.Entry<String, Object> entry : sortedMap1.entrySet()) {
-            String key = entry.getKey();
-            Object value1 = entry.getValue();
-            Object value2 = sortedMap2.get(key);
-            if (value2 == null) {
-                result.add("  - " + key + ": " + value1);
-            } else if (value1.equals(value2)) {
-                result.add("    " + key + ": " + value1);
-            } else {
-                result.add("  - " + key + ": " + value1);
-                result.add("  + " + key + ": " + value2);
-            }
-        }
-        for (Map.Entry<String, Object> entry : sortedMap2.entrySet()) {
-            String key = entry.getKey();
-            if (sortedMap1.get(key) == null) {
-                result.add("  + " + key + ": " + entry.getValue());
-            }
-        }
-        return result;
+        return generate(file1, file2, "stylish");
     }
 }
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -8,31 +8,34 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
-import org.apache.commons.io.FilenameUtils;
 
 public class Parser {
+    public static Map<String, Object> parse(String filePath) throws IOException {
+        Path pathToFile = Path.of(filePath);
+        String extension = getFileExtension(pathToFile);
+
+        return switch (extension) {
+            case "json" -> parseJson(pathToFile);
+            case "yml", "yaml" -> parseYaml(pathToFile);
+            default -> throw new IllegalArgumentException("Данный формат не поддерживается!");
+        };
+    }
+
+    private static String getFileExtension(Path path) {
+        String fileName = path.getFileName().toString();
+        int dotIndex = fileName.lastIndexOf('.');
+        return (dotIndex == -1) ? "" : fileName.substring(dotIndex + 1);
+    }
+
     private static Map<String, Object> parseJson(Path filepath) throws IOException {
         String json = Files.readString(filepath);
         ObjectMapper mapper = new ObjectMapper();
-
-        return mapper.readValue(json, new TypeReference<Map<String, Object>>() { });
+        return mapper.readValue(json, new TypeReference<>() {});
     }
 
     private static Map<String, Object> parseYaml(Path filepath) throws IOException {
         String yaml = Files.readString(filepath);
         ObjectMapper mapper = new YAMLMapper();
-
-        return mapper.readValue(yaml, new TypeReference<Map<String, Object>>() { });
-    }
-
-    public static Map<String, Object> parser(String filePath) throws IOException {
-        Path pathToFile = Path.of(filePath);
-        String extension = FilenameUtils.getExtension(pathToFile.toString());
-
-        return switch (extension) {
-            case "json" -> parseJson(pathToFile);
-            case "yml" -> parseYaml(pathToFile);
-            default -> throw new Error("Данный формат не поддерживается!");
-        };
+        return mapper.readValue(yaml, new TypeReference<>() {});
     }
 }
