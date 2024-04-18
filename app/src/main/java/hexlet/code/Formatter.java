c0e91f44cfe9ba85d535f272f1209a3a3e682dd1
Alexander <nekon689@gmail.com>
2024-04-18 11:26:33 +0300

    add json formatter

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,5 +1,7 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import hexlet.code.formatters.Json;
 import hexlet.code.formatters.Plain;
 import hexlet.code.formatters.Stylish;
 
@@ -7,10 +9,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Formatter {
-    public static String format(ArrayList<HashMap<String, Object>> diffObj, String titleFormat) {
+    public static String format(ArrayList<HashMap<String, Object>> diffObj, String titleFormat) throws JsonProcessingException {
         return switch (titleFormat) {
             case "stylish" -> Stylish.formatDiff(diffObj);
             case "plain" -> Plain.formatDiff(diffObj);
+            case "json" -> Json.formatDiff(diffObj);
             default -> throw new Error("Данный формат не поддерживается!");
         };
     }
