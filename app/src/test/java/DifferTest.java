4f1da15033a9a86c01faf3c08eea54f9ae5d3aa7
Alexander <nekon689@gmail.com>
2024-04-18 13:38:30 +0300

    fix resultdiff

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -71,7 +71,7 @@ public class DifferTest {
         String expectedDiff = "Property 'chars2' was updated. From [complex value] to false\n"
                 + "Property 'checked' was updated. From false to true\n"
                 + "Property 'default' was updated. From null to [complex value]\n"
-                + "Property 'id' was updated. From '45' to null\n"
+                + "Property 'id' was updated. From 45 to null\n"
                 + "Property 'key1' was removed\n"
                 + "Property 'key2' was added with value: 'value2'\n"
                 + "Property 'numbers2' was updated. From [complex value] to [complex value]\n"
@@ -79,7 +79,7 @@ public class DifferTest {
                 + "Property 'numbers4' was added with value: [complex value]\n"
                 + "Property 'obj1' was added with value: [complex value]\n"
                 + "Property 'setting1' was updated. From 'Some value' to 'Another value'\n"
-                + "Property 'setting2' was updated. From '200' to '300'\n"
+                + "Property 'setting2' was updated. From 200 to 300\n"
                 + "Property 'setting3' was updated. From true to 'none'";
 
         String actualDiff = Differ.generate("src/test/resources/file3.json", "src/test/resources/file4.json", "plain");
